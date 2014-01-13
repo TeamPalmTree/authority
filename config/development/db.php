2093b8cdc4598706c4b9487dca4bf72a7f075eb8
Alex Groleau <alex@teampalmtree.com>
2014-01-13 14:20:02 -0500

    git ignores updated dev db file added global promoter config added for app

---
+++ config/development/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'default' => array(
+        'connection'  => array(
+            'dsn'        => 'mysql:host=authority;dbname=authority',
+            'username'   => 'root',
+            'password'   => '',
+        ),
+    ),
+);
